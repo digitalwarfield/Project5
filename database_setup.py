824a27305b3bb70ecc2a9b456e3a6f311a88cb16
digitalwarfield <jswarfield@gmail.com>
2019-01-31 08:57:08 -0700

    Adding Users table and switching to postrgres

--- database_setup.py
+++ database_setup.py
@@ -15,11 +15,23 @@ from datetime import datetime
 Base = declarative_base()
 
 
+class Users(Base):
+    __tablename__ = 'users'
+    email = Column(String(256)),
+                   nullable=False,
+                   unique=True)
+    user_id = Column(Integer, primary_key=True)
+    picture = Column(Text)
+    full_name = Column(String(256))
+
+
 class Categories(Base):
     __tablename__ = 'categories'
     # Only allow for one category to be added regardless of case
-    name = Column(String(128, collation='NOCASE'), nullable=False, unique=True)
+    name = Column(String(100)), nullable=False, unique=True)
     cat_id = Column(Integer, primary_key=True)
+    users = relationship(Users)
+    user_id = Column(Integer, ForeignKey('users.user_id'))
     @property
     def serialize(self):
         # Returns object data in easily serializeable format
@@ -31,7 +43,7 @@ class Categories(Base):
 
 class Items(Base):
     __tablename__ = 'items'
-    title = Column(String(80, collation='NOCASE'), nullable=False)
+    title = Column(String(80)), nullable=False)
     item_id = Column(Integer, primary_key=True)
     description = Column(Text)
     last_update = Column(DateTime(timezone=True),
@@ -39,6 +51,8 @@ class Items(Base):
                          onupdate=datetime.now())
     cat_id = Column(Integer, ForeignKey('categories.cat_id'))
     categories = relationship(Categories)
+    users = relationship(Users)
+    user_id = Column(Integer, ForeignKey('users.user_id'))
 
     @property
     def serialize(self):
@@ -53,7 +67,21 @@ class Items(Base):
 
 # This is used to only allow one title per category
 Index('uniqueTitlePerCategory', Items.title, Items.cat_id, unique=True)
-engine = create_engine('sqlite:///catalog.db')
+
+
+try:
+    database_login_file = open("/var/www/html/app_config.json", "r")
+    database_login = json.load(database_login_file)
+    user = database_login["db_user"]
+    password = database_login["db_pass"]
+    database = database_login["db_database"]
+except Exception:
+    print("Failed to open database login file")
+    raise
+
+
+database_params = "postgresql://{}:{}@localhost/{}".format(user,password,database)
+engine = create_engine(database_params)
 
 
 Base.metadata.create_all(engine)
